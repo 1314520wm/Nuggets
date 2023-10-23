0d13fbc63cbf498edff88e91aa84bc7a6fd098aa
1314520wm <84711505+1314520wm@users.noreply.github.com>
2023-10-23 14:06:56 +0800

    Update cookie.js

--- src/cookie.js
+++ src/cookie.js
@@ -1,15 +1,13 @@
 const COOKIE = {
-    "__tea_cookie_tokens_2608": "%257B%2522web_id%2522%253A%25227246965070184531495%2522%252C%2522user_unique_id%2522%253A%25227246965070184531495%2522%252C%2522timestamp%2522%253A1687315560289%257D",
-    "passport_csrf_token": "4ba55c83ad9f803ffbe48817bbc7f741",
-    "passport_csrf_token_default": "4ba55c83ad9f803ffbe48817bbc7f741",
-    "csrf_session_id": "313e7b3299f52f2e7c538b80afc8bec0",
-    "msToken": "Auvfmmhsxy6ndkepU95i0rBY2t3dyO4LpaFRjTK6QN54XxDJit7B5xu_VpVoM8QOIVdndcMrz0EKmWkXw0k4P2JrMd0QGn2STQmTqz01YviUXl-B8WVpvs1IJGDwlQ==",
-    "n_mh": "m4Nwod14ocYxZ8VeMr34-AMseNqHnd0sqChhin9oDYw",
-    "sid_guard": "165f21943064fd6d7de7341ab0a15c2c%7C1690875628%7C31536000%7CWed%2C+31-Jul-2024+07%3A40%3A28+GMT",
-    "sid_tt": "165f21943064fd6d7de7341ab0a15c2c",
-    "uid_tt": "265f8aa699848264f68e5381f1595040",
-    "uid_tt_ss": "265f8aa699848264f68e5381f1595040"
+"csrf_session_id": "4837108d885ea5db68a18225f8eacf97",
+  "msToken": "h6G8P32nasj9MUfFIj_XVo9zQI40F_ySq1T75zpTCi9vvRJ7nHi6SrFd9OwtYl_Ei5H612xi-GmFY1xj5vt7KUfXlMP8oKA9l_DTvB91g_ktUfMTyQRVS_kbEhrYKA==",
+  "__tea_cookie_tokens_2608": "%257B%2522web_id%2522%253A%25227051826796736398855%2522%252C%2522ssid%2522%253A%2522d77c7541-3b38-494f-8f4d-7373eaf7d654%2522%252C%2522user_unique_id%2522%253A%25227051826796736398855%2522%252C%2522timestamp%2522%253A1641881380020%257D",
+  "uid_tt_ss": "1fc336503395a4ec4645b9e3e5b5c9e8",
+  "uid_tt": "1fc336503395a4ec4645b9e3e5b5c9e8",
+  "sid_guard": "1a0f821912ec50d926ce8a5fcf786f23%7C1673073313%7C31536000%7CSun%2C+07-Jan-2024+06%3A35%3A13+GMT",
+  "sid_tt": "1a0f821912ec50d926ce8a5fcf786f23"
 }
+
 
 module.exports = {
     COOKIE
